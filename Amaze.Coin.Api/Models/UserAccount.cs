3d491725efed806633144dd28472045c2101e7fd
ffabrizio <f.fabrizio@amaze.com>
2018-01-04 01:32:18 +0000

    VS/Rider compatibility

---
+++ Amaze.Coin.Api/Models/UserAccount.cs
@@ -0,0 +1,23 @@
+﻿using NBitcoin;
+using Nethereum.HdWallet;
+
+namespace Amaze.Coin.Api.Models
+{
+    public class UserAccount
+    {
+        public string UserName { get; private set; }
+        public string DisplayName { get; private set; }
+        public Wallet Wallet { get; private set; }
+        
+        public static UserAccount Initialize(string userName, string displayName = null)
+        {
+            return new UserAccount
+            {
+                UserName = userName,
+                DisplayName = displayName,
+                Wallet = new Wallet(Wordlist.English, WordCount.Twelve)
+            };
+        }
+
+    }
+}
