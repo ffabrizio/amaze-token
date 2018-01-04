5b746a407348e9b7dd92ef1682d7112b52b5b11e
ffabrizio <f.fabrizio@amaze.com>
2018-01-04 01:47:02 +0000

    Missing Interface

---
+++ Amaze.Coin.Api/Stores/IAccountStore.cs
@@ -0,0 +1,11 @@
+﻿using Amaze.Coin.Api.Models;
+
+namespace Amaze.Coin.Api.Stores
+{
+    public interface IAccountStore
+    {
+        UserAccount GetAccount(string userName);
+        UserAccount AddAccount(string userName);
+        int GetBalance(string address);
+    }
+}
